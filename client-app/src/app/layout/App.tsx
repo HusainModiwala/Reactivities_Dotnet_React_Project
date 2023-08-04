8d848e2090f40532955b73316776865284cb46a2
HusainModiwala <hussainmodiwala@gmail.com>
2023-08-04 19:52:57 +0530

    added forms to create and edit activities using in react using semantic-ui

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,6 +7,9 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
     .then(response => {
@@ -14,11 +17,35 @@ function App() {
       setActivities(response.data)
     })
   }, [])
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+  
+  function handleFormOpen(id?: string) {
+    id? handleSelectActivity(id) : handleCancelSelectActivity()
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
   return (
     <>
-      <NavBar/>
+      <NavBar openForm = {handleFormOpen}/>
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard activities = {activities}/>
+        <ActivityDashboard activities = {activities}
+        selectedActivity = {selectedActivity} 
+        selectActivity = {handleSelectActivity} 
+        cancelSelectActivity = {handleCancelSelectActivity} 
+        editMode = {editMode}
+        openForm = {handleFormOpen}
+        closeForm = {handleFormClose}/>
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../forms/ActivityForm";
 
 interface Props{
     activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean
+    openForm: (id?: string) => void
+    closeForm: () => void
 }
 
-export default function ActivityDashboard({activities} : Props) {
+export default function ActivityDashboard({activities, selectedActivity, selectActivity, 
+  cancelSelectActivity, editMode, openForm, closeForm} : Props) {
   return (
     <Grid>
       <Grid.Column width="10">
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityList activities={activities}
+        selectActivity = {selectActivity}/>
+      </Grid.Column>
+      <Grid.Column width="6">
+        {selectedActivity &&
+        <ActivityDetails activity={selectedActivity} 
+        cancelSelectActivity = {cancelSelectActivity}
+        openForm = {openForm}/>}
+        {editMode && <ActivityForm selectedActivity = {selectedActivity} closeForm = {closeForm}/>}
       </Grid.Column>
     </Grid>
   );
